55373a70b31ccc2f9298db0723d4d27c8efd7d47
Samuel Otterman <sjotterman@gmail.com>
2014-04-04 14:26:10 -0400

    Modified runCube to print out results of a few right cw rotations

--- src/runCube.py
+++ src/runCube.py
@@ -6,5 +6,18 @@ def main():
    testCube = cube.Cube()
    testCube.print_cube() 
 
+   print("Rotating the right side clockwise.")
+   testCube.move('right','cw') 
+   testCube.print_cube() 
+   print("Rotating the right side clockwise.")
+   testCube.move('right','cw') 
+   testCube.print_cube() 
+   print("Rotating the right side clockwise.")
+   testCube.move('right','cw') 
+   testCube.print_cube() 
+   print("Rotating the right side clockwise.")
+   testCube.move('right','cw') 
+   testCube.print_cube() 
+
 if __name__ == "__main__":
     main()
