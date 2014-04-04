d84c76030ff7a13f723e03bc5a671cc0ea1e3b24
Samuel Otterman <sjotterman@gmail.com>
2014-04-04 15:46:45 -0400

    Added some reorient(up) calls in runCube

--- src/runCube.py
+++ src/runCube.py
@@ -19,5 +19,18 @@ def main():
    testCube.move('right','cw') 
    testCube.print_cube() 
 
+   print("Here's a new cube:")
+   secondCube = cube.Cube()
+   secondCube.print_cube()
+   print("Reorient up")
+   secondCube.reorient("up")
+   secondCube.print_cube()
+   print("Rotating the right side clockwise.")
+   secondCube.move('right', 'cw') 
+   secondCube.print_cube()
+   print("Reorient up")
+   secondCube.reorient("up")
+   secondCube.print_cube()
+
 if __name__ == "__main__":
     main()
