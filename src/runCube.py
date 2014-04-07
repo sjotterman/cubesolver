5014ff3d48eb73a29de88146f88c1dd714a0edc0
Samuel Otterman <sjotterman@gmail.com>
2014-04-07 15:51:11 -0400

    Changed runCube to debug move('front','right')

--- src/runCube.py
+++ src/runCube.py
@@ -4,15 +4,17 @@ import cube
 
 def main():
    testCube = cube.Cube()
-   testCube.print_cube() 
+   testCube.print_cube()
+   print("front lup")
+   testCube.move('front', 'lup')
+   testCube.print_cube()
+   print("front right")
+   testCube.move('front', 'right')
+   testCube.print_cube()
+   
 
-   testCube.print_cube()
-   print("front rup")
-   testCube.move('front', 'rup')
-   testCube.print_cube()
-   print("front tright")
-   testCube.move('front', 'tright')
-   testCube.print_cube()
+print
+
 
 if __name__ == "__main__":
     main()
