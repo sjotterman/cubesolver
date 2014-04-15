da8c6120e005231a1c6e0e03b116fbbc01b3295c
Samuel Otterman <sjotterman@gmail.com>
2014-04-15 17:18:19 -0400

    Modified runCube to find bug in switching two blue cubes

--- src/runCube.py
+++ src/runCube.py
@@ -5,25 +5,16 @@ import cube
 def main():
    testCube = cube.Cube()
    testCube.print_cube()
-   testCube.reorient('up')
-   testCube.move('front','down')
-   testCube.move('front','bright')
-   testCube.move('front','up')
-   testCube.reorient('left')
-   testCube.move('front','ccw')
-   testCube.move('front','left')
-   testCube.move('front','cw')
-   testCube.reorient('right')
-   testCube.move('front', 'left')
-   testCube.move('front','ccw')
-   testCube.move('middle', 'right')
-   testCube.move('front', 'cw')
-
-   print("Switch two blue cubes")
+   print("Front tright")
+   testCube.move('front','tright')
    testCube.print_cube()
-   
+   print("Front down")
+   testCube.move('front','down')
+   testCube.print_cube()
+   print("Front left")
+   testCube.move('front','left')
+   testCube.print_cube()
 
-print
 
 
 if __name__ == "__main__":
