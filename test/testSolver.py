d5d2388b9d13f880a65e3d07a4151adf33d48a54
Samuel Otterman <sjotterman@gmail.com>
2014-05-04 19:42:16 -0400

    Fixed formatting in testSolver

--- test/testSolver.py
+++ test/testSolver.py
@@ -2,8 +2,12 @@
 
 import unittest
 from ..src import solver
+from ..src import cube
 
 class TestSolver(unittest.TestCase):
-  def test_createSolver(self):
-    mySolver = solver.Solver()
+
+    def test_createSolver(self):
+        mySolver = solver.Solver()
+
+
 
