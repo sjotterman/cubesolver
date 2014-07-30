78dcc0ca4622dec3e8e3d2829ceb484b442f1cc0
Samuel Otterman <sjotterman@gmail.com>
2014-07-30 09:02:31 -0400

    Whitespace/formatting for solver.py

--- src/solver.py
+++ src/solver.py
@@ -2,6 +2,7 @@
 
 import cube
 
+
 class Solver(object):
 
     def __init__(self):
@@ -17,10 +18,10 @@ class Solver(object):
         backColor = target.face['back'][4]
         loopCounter = 0
         while target.isFirstLayerSolved(topColor) == False:
-            for i in range(0,4):
+            for i in range(0, 4):
                 target.move('front', 'bleft')
-                
-                # front bottom left square needs to go to top 
+
+                # front bottom left square needs to go
                 if target.face['front'][6] == topColor:
                     if (target.face['left'][8] == leftColor) \
                         and (target.face['bottom'][0] == frontColor):
@@ -28,7 +29,7 @@ class Solver(object):
                         target.move('front', 'ldown')
                         target.move('front', 'bleft')
                         target.move('front', 'lup')
-                # front bottom right square needs to go to top 
+                # front bottom right square needs to go to top
                 elif target.face['front'][8] == topColor:
                     if (target.face['right'][6] == rightColor) \
                         and (target.face['bottom'][2] == frontColor):
@@ -36,16 +37,15 @@ class Solver(object):
                         target.move('front', 'rdown')
                         target.move('front', 'bright')
                         target.move('front', 'rup')
-                    #stuff
-                # front bottom middle square needs to go to top 
+                # front bottom middle square needs to go to top
                 elif target.face['front'][7] == topColor:
                     if (target.face['bottom'][1] == frontColor):
                         target.move('front', 'bright')
                         target.move('front', 'down')
                         target.move('front', 'bleft')
                         target.move('front', 'up')
-                            
-                # bottom top left square needs to go to top 
+
+                # bottom top left square needs to go to top
                 elif target.face['bottom'][0] == topColor:
                     if (target.face['left'][8] == frontColor) \
                         and (target.face['front'][6] == leftColor):
@@ -58,8 +58,7 @@ class Solver(object):
                         target.move('front', 'bright')
                         target.move('front', 'lup')
 
-
-                # bottom top middle square needs to go to top 
+                # bottom top middle square needs to go to top
                 elif target.face['bottom'][1] == topColor:
                     if target.face['front'][7] == frontColor:
                         target.move('front', 'ccw')
@@ -69,7 +68,7 @@ class Solver(object):
                         target.move('front', 'left')
                         target.move('front', 'ccw')
 
-                # bottom top right square needs to go to top 
+                # bottom top right square needs to go to top
                 elif target.face['bottom'][2] == topColor:
                     if (target.face['right'][6] == frontColor) \
                         and (target.face['front'][8] == rightColor):
@@ -81,7 +80,7 @@ class Solver(object):
                         target.move('front', 'rdown')
                         target.move('front', 'bleft')
                         target.move('front', 'rup')
-                
+
                 # front top right square needs to go to top, just rotate it
                 # down, it will be handled
                 elif target.face['front'][2] == topColor:
@@ -109,18 +108,18 @@ class Solver(object):
                     if target.face['top'][6] == topColor:
                         if target.face['front'][0] != frontColor \
                             or target.face['left'][2] != leftColor:
-                            target.move('front','ldown')
+                            target.move('front', 'ldown')
                             target.move('front', 'bright')
                             target.move('front', 'lup')
                     if target.face['top'][7] == topColor:
                         if target.face['front'][1] != frontColor:
-                            target.move('front','down')
+                            target.move('front', 'down')
                             target.move('front', 'bright')
                             target.move('front', 'up')
                     if target.face['top'][8] == topColor:
                         if target.face['front'][2] != frontColor \
                             or target.face['right'][0] != rightColor:
-                            target.move('front','rdown')
+                            target.move('front', 'rdown')
                             target.move('front', 'bleft')
                             target.move('front', 'rup')
 
@@ -140,11 +139,6 @@ class Solver(object):
                     target.move('front', 'left')
                     target.move('front', 'cw')
                 target.move('front', 'left')
-                
-
-
-
-
 
             target.reorient('left')
             topColor = target.face['top'][4]
@@ -154,9 +148,8 @@ class Solver(object):
             backColor = target.face['back'][4]
             loopCounter += 1
 
-
             if loopCounter == 4000:
-                print ("First layer Loop Counter exceeded max.")
+                print("First layer Loop Counter exceeded max.")
                 return False
 
         loopCounter = 0
@@ -171,7 +164,6 @@ class Solver(object):
 
         while target.face['front'][7] != target.face['front'][4]:
             target.move('front', 'left')
-
 
         while target.isSecondLayerSolved(target.face['bottom'][4]) == False:
             centerColor = target.face['front'][4]
@@ -188,7 +180,7 @@ class Solver(object):
                     target.move('front', 'ccw')
                     target.move('front', 'tleft')
                     target.move('front', 'cw')
-                    
+
                 # move to front 3
                 elif target.face['top'][7] == target.face['left'][4]:
                     target.move('front', 'tright')
@@ -199,8 +191,6 @@ class Solver(object):
                     target.move('front', 'cw')
                     target.move('front', 'tright')
                     target.move('front', 'ccw')
-
-
 
             if loopCounter % 4 == 0:
                 if target.face['front'][3] != target.face['top'][4]:
@@ -232,10 +222,8 @@ class Solver(object):
 
             loopCounter += 1
 
-
-
             if loopCounter == 4000:
-                print ("Second Layer Loop Counter exceeded max.")
+                print("Second Layer Loop Counter exceeded max.")
                 target.print_cube()
                 return False
 
@@ -249,4 +237,3 @@ class Solver(object):
         while target.face['front'][4] != startFrontColor:
             target.reorient('left')
         return True
-    
